4ddf39d985d4181b204501fffc6fa3a74b9fb169
harkato <92404938+harkato@users.noreply.github.com>
2023-07-26 18:28:27 -0300

    Create grid and hover effect

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+const container = document.querySelector(".container");
+
+function createGrid() {
+    for( let i = 0; i < 16; i++ ){
+        for( let j = 0; j < 16; j++ ){
+            const grid = document.createElement("div");
+            grid.classList.add("gridElement");
+            container.appendChild(grid);
+        }
+    }
+}
+
+function changeGridElementColor(e) {
+    e.target.classList.add("mouseEvent");
+}
+console.log(createGrid());
+
+const gridElements = Array.from(document.querySelectorAll('.gridElement'));
+gridElements.forEach(gridElement => gridElement.addEventListener('mouseover', changeGridElementColor));
