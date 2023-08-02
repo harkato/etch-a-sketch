e59468290b8dcb891d2dc2f582fa4f5d801979b1
harkato <92404938+harkato@users.noreply.github.com>
2023-08-02 17:57:33 -0300

    Make button to change grid size

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
 const container = document.querySelector(".container");
 
-function createGrid() {
-    for( let i = 0; i < 16; i++ ){
-        for( let j = 0; j < 16; j++ ){
-            const grid = document.createElement("div");
-            grid.classList.add("gridElement");
-            container.appendChild(grid);
-        }
-    }
-}
-
 function changeGridElementColor(e) {
     e.target.classList.add("mouseEvent");
 }
-console.log(createGrid());
 
-const gridElements = Array.from(document.querySelectorAll('.gridElement'));
-gridElements.forEach(gridElement => gridElement.addEventListener('mouseover', changeGridElementColor));
+function newGrid() {
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+    let gridSize = prompt("Number of squares per side for the new grid (Max: 100)", "16");
+    gridSize = parseInt(gridSize, 10);
+    let gridWidth = 100/gridSize;
 
-console.log(100/18)
+    if (!isNaN(gridSize) && gridSize < 100) {
+        let maxRow = gridSize;
+        let maxCol = gridSize;
+        for( let i = 0; i < maxRow; i++ ){
+            for( let j = 0; j < maxCol; j++ ){
+                const grid = document.createElement("div");
+                grid.classList.add("gridElement");
+                container.appendChild(grid);
+            }
+        }
+        const gridElements = Array.from(document.querySelectorAll('.gridElement'));
+        gridElements.forEach( (gridElement) => {
+            gridElement.style.flex = `0 0 ${gridWidth}%`;
+            gridElement.style.paddingTop = `${gridWidth}%`;
+        })
+        gridElements.forEach(gridElement => gridElement.addEventListener('mouseover', changeGridElementColor));
+    }
+
+}
+
+
+const button = document.getElementById("button");
+button.addEventListener("click", newGrid);
+
+
